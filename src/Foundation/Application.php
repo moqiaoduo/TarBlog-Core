b55a608db52dd55b829326c3af7a18e36f966d19
moqiaoduo <415372363@qq.com>
2020-03-28 20:34:49 +0800

    加回encrypt

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -883,6 +883,7 @@ class Application extends Container implements ApplicationContract
                      'cookie'               => [\Illuminate\Cookie\CookieJar::class, \Illuminate\Contracts\Cookie\Factory::class, \Illuminate\Contracts\Cookie\QueueingFactory::class],
                      'db'                   => [\Illuminate\Database\DatabaseManager::class, \Illuminate\Database\ConnectionResolverInterface::class],
                      'db.connection'        => [\Illuminate\Database\Connection::class, \Illuminate\Database\ConnectionInterface::class],
+                     'encrypter'            => [\Illuminate\Encryption\Encrypter::class, \Illuminate\Contracts\Encryption\Encrypter::class],
                      'events'               => [\Illuminate\Events\Dispatcher::class, \Illuminate\Contracts\Events\Dispatcher::class],
                      'files'                => [\Illuminate\Filesystem\Filesystem::class],
                      'filesystem'           => [\Illuminate\Filesystem\FilesystemManager::class, \Illuminate\Contracts\Filesystem\Factory::class],
