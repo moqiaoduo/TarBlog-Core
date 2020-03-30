756c27e164a34ba0c216d311552e8b7e6041d9d0
moqiaoduo <415372363@qq.com>
2020-03-30 15:41:36 +0800

    删除不用的action方法

--- src/Foundation/helpers.php
+++ src/Foundation/helpers.php
@@ -83,21 +83,6 @@ if (! function_exists('abort_unless')) {
     }
 }
 
-if (! function_exists('action')) {
-    /**
-     * Generate the URL to a controller action.
-     *
-     * @param  string|array  $name
-     * @param  mixed  $parameters
-     * @param  bool  $absolute
-     * @return string
-     */
-    function action($name, $parameters = [], $absolute = true)
-    {
-        return app('url')->action($name, $parameters, $absolute);
-    }
-}
-
 if (! function_exists('app')) {
     /**
      * Get the available container instance.
